0c9d9e10b063f67158fafdada36afba6b69888ed
parnuluck <parnuluck.s@gmail.com>
2026-03-22 03:28:38 +0700

    Create 4_Test_NN.py

---
+++ pages/4_Test_NN.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Neural Network Model")
+st.subheader("Enter patient data")
+st.write("Neural Network model is used in this project.")
+st.write("Due to deployment limitations, this demo is not available.")
