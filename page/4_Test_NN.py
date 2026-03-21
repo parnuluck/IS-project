33b48f38d1ed5e6e83e385de46a10d0aa4f5bf14
parnuluck <parnuluck.s@gmail.com>
2026-03-22 03:28:59 +0700

    Delete page directory

--- page/4_Test_NN.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Neural Network Model")
-st.subheader("Enter patient data")
-st.write("Neural Network model is used in this project.")
-st.write("Due to deployment limitations, this demo is not available.")
