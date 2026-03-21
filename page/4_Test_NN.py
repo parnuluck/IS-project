47482309d20e1138334ed892008c36828f169f11
parnuluck <parnuluck.s@gmail.com>
2026-03-22 02:55:41 +0700

    Update 4_Test_NN.py

--- page/4_Test_NN.py
+++ page/4_Test_NN.py
@@ -1,20 +1,6 @@
 import streamlit as st
-import numpy as np
-from tensorflow.keras.models import load_model
 
-st.title("Test Neural Network")
+st.title("Neural Network Model")
 
-model = load_model("nn_model.h5")
-
-inputs = []
-for col in ["Pregnancies","Glucose","BloodPressure","SkinThickness",
-            "Insulin","BMI","DiabetesPedigreeFunction","Age"]:
-    inputs.append(st.number_input(col))
-
-if st.button("Predict NN"):
-    result = model.predict([inputs])
-
-    if result[0][0] > 0.5:
-        st.error("Diabetes")
-    else:
-        st.success("No Diabetes")
+st.write("Neural Network model is used in this project.")
+st.write("Due to deployment limitations, this demo is not available.")
