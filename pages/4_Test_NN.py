7b569984f1bbaea4f5323dfc96a0866cfface2d6
parnuluck <parnuluck.s@gmail.com>
2026-03-22 16:09:33 +0700

    Update 4_Test_NN.py

--- pages/4_Test_NN.py
+++ pages/4_Test_NN.py
@@ -1,32 +1,27 @@
 import streamlit as st
 import pickle
 import re
-from tensorflow.keras.models import load_model
 
 # load model
-model = load_model("nn_model.h5")
+model = pickle.load(open("nn_model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
-# load vectorizer
-with open("vectorizer.pkl", "rb") as f:
-    vectorizer = pickle.load(f)
-
-# clean text
 def clean_text(text):
     text = text.lower()
     text = re.sub(r"<.*?>", "", text)
     text = re.sub(r"[^a-zA-Z]", " ", text)
     return text
 
-st.title("Sentiment Analysis (IMDb)")
+st.title("Neural Network (Sentiment Analysis)")
 
-review = st.text_area("Enter your review")
+review = st.text_area("Enter movie review")
 
 if st.button("Predict"):
     cleaned = clean_text(review)
-    vec = vectorizer.transform([cleaned]).toarray()
-    pred = model.predict(vec)[0][0]
+    vec = vectorizer.transform([cleaned])
+    pred = model.predict(vec)[0]
 
-    if pred > 0.5:
+    if pred == 1:
         st.success("Positive 😊")
     else:
         st.error("Negative 😡")
