ed4136db010272eeaefc2e8a36d9d23505f6e7cb
parnuluck <parnuluck.s@gmail.com>
2026-03-24 00:44:34 +0700

    Update 4_Test_NN.py

--- pages/4_Test_NN.py
+++ pages/4_Test_NN.py
@@ -17,16 +17,6 @@ st.title("Neural Network (Sentiment Analysis)")
 review = st.text_area("Enter movie review")
 
 if st.button("Predict"):
-    cleaned = clean_text(review)
-    vec = vectorizer.transform([cleaned])
-    pred = model.predict(vec)[0]
-
-    if pred == 1:
-        st.success("Positive 😊")
-    else:
-        st.error("Negative 😡")
-
-  if st.button("Predict"):
     if review.strip() == "":
         st.warning("Please enter a review")
     else:
